5efa7af4668d5aacd1b81daa1cbe4dbd6f3c6e16
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2019-03-01 16:35:00 +0100

    adding 4th parametar to the array

--- anothersolution.js
+++ anothersolution.js
@@ -4,72 +4,95 @@ const background = PIXI.Sprite.fromImage("images/background.png");
 const movingGuy = PIXI.Sprite.fromImage("images/guy.png");
 movingGuy.x = 660;
 movingGuy.y = 70;
-window.reverse = true;
+window.reverse = false;
 window.currentIndex = 0;
 window.movingGuyInterval = null;
 app.stage.addChild(background);
 app.stage.addChild(movingGuy);
 let waypoints = [
-  [680, 70, true],
-  [680, 440, true],
-  [125, 440, false],
-  [125, 110, false],
-  [640, 110, true],
-  [640, 400, true],
-  [165, 400, false],
-  [165, 150, false],
-  [600, 150, true],
-  [600, 360, true],
-  [205, 360, false],
-  [205, 190, false],
-  [560, 190, true],
-  [560, 330, true],
-  [240, 330, false],
-  [240, 220, false],
-  [530, 220, true],
-  [530, 300, true],
-  [295, 300, false]
+  [680, 70, true, 'x'],
+  [680, 440, true, 'y'],
+  [125, 440, false, 'x'],
+  [125, 110, false, 'y'],
+  [640, 110, true, 'x'],
+  [640, 400, true, 'y'],
+  [165, 400, false, 'x'],
+  [165, 150, false, 'y'],
+  [600, 150, true, 'x'],
+  [600, 360, true, 'y'],
+  [205, 360, false, 'x'],
+  [205, 190, false, 'y'],
+  [560, 190, true, 'x'],
+  [560, 330, true, 'y'],
+  [240, 330, false, 'x'],
+  [240, 220, false, 'y'],
+  [530, 220, true, 'x'],
+  [530, 300, true, 'y'],
+  [295, 300, false, 'x'],
 ];
 
-// function reverseArray() {
-//   console.log('whyyyyyy')
-//   waypoints.map(waypoint => {
-//     waypoint[2] = !waypoint[2];
-//   })
-//   waypoints.reverse();
-//   console.log(waypoints);
-// }
+function reverseArray() {
+  window.reverse = !window.reverse;
+  waypoints.map(waypoint => {
+    waypoint[2] = !waypoint[2];
+  })
+  if (window.reverse) {
+    count = count - 1;
+    console.log(count);
+  }
+  else if (!window.reverse) {
+    count = count + 1;
+    console.log(count);
+  }
+  // waypoints.reverse();
+}
 
 function setup() {
   app.ticker.add(delta => gameLoop(delta));
 }
 
+//GET INDEX
 var count = 0;
-state = true;
-pos = "x";
+// state = true;
+// pos = "x";
 function gameLoop(delta) {
+  console.log(count)
+  // console.log(movingGuy.x);
+  // console.log(movingGuy.y);
   if (waypoints[count][2]) {
-    movingGuy[pos] = movingGuy[pos] + 5;
+    movingGuy[waypoints[count][3]] = movingGuy[waypoints[count][3]] + 5;
     if (
       movingGuy.x == waypoints[count][0] &&
       movingGuy.y == waypoints[count][1]
     ) {
-      count = count + 1;
-      state = !state;
+      if (window.reverse) {
+        count = count - 1;
+      }
+      else if (!window.reverse) {
+        count = count + 1;
+      }
+      // count = count + 1;
+      // state = !state;
     }
   } else if (!waypoints[count][2]) {
-    movingGuy[pos] = movingGuy[pos] - 5;
+    movingGuy[waypoints[count][3]] = movingGuy[waypoints[count][3]] - 5;
     if (
       movingGuy.x == waypoints[count][0] &&
       movingGuy.y == waypoints[count][1]
     ) {
-      count = count + 1;
-      state = !state;
+      if (window.reverse) {
+        count = count - 1;
+      }
+      else if (!window.reverse) {
+        count = count + 1;
+      }
+      // count = count + 1;
+      // state = !state;
     }
   }
-  if (state) {
-    pos = "x";
-  } else if (!state) {
-    pos = "y";
-  }
+  // if (state) {
+  //   pos = "x";
+  // } else if (!state) {
+  //   pos = "y";
+  // }
 }
